24a242da34b043eb0e4fa8c9640df75911f32ceb
anu763 <anu763@yahoo.com>
2020-11-13 20:00:10 -0500

    updated
    updating

---
+++ Anagrams.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Anagrams {
+    static boolean isAnagram(String a, String b) {
+        Map<Character, Integer> first = new HashMap<>();
+        Map<Character, Integer> sec = new HashMap<>();
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        char[] firstArr = a.toCharArray();
+        char[] secArr = b.toCharArray();
+        
+        for(int i=0; i< firstArr.length; i++){
+        int val =  (first.get(firstArr[i]) != null) ? (first.get(firstArr[i]) +1) : 1; 
+            first.put(firstArr[i], val );            
+        }
+        for(int i=0; i< secArr.length; i++){
+        int val =  (sec.get(secArr[i]) != null) ? (sec.get(secArr[i]) +1) : 1; 
+            sec.put(secArr[i], val );            
+        }
+        if(first.size() == sec.size()){
+            for(Map.Entry<Character, Integer> entry : first.entrySet() ){
+                if(sec.get(entry.getKey()) != entry.getValue()){
+                    return false;
+                }
+            }
+        }else{
+            return false;
+        }
+        return true;
+     }
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
--- BinarySearch.java
+++ BinarySearch.java
@@ -1,7 +1,13 @@
+import java.util.UUID;
 
 public class BinarySearch {
 	
 	public static void main(String args[]) {
+		
+		 UUID uuid = UUID.randomUUID();
+		 System.out.println("uuid.toString() ===>>> "+ uuid.toString());
+		 uuid = UUID.randomUUID();
+		 System.out.println("uuid.toString() ===>>> "+ uuid.toString());
 		int [] sortedArr = {1,2,3,4,5,6,7,8,9,10};
 		
 		int quo = searchNumber(sortedArr, 0, 9, 6);
